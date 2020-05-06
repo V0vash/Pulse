c8f4c2f0d9b1812587aef376f4a65a73042fed1d
Vladimir <avevanguard@gmail.com>
2020-05-07 01:26:20 +0300

    added slider + tabs

---
+++ src/js/script.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 1200,
+        adaptiveHeight: true,
+        prevArrow:'<button type="button" class="slick-prev"><img src="icons/chevron-left-solid.svg"></button>',
+        nextArrow:'<button type="button" class="slick-next"><img src="icons/chevron-right-solid.svg"></button>',
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                  dots: true,
+                  arrows: false
+                }
+              },
+        ]
+      });
+
+
+$('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+  $(this)
+    .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+    .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+});
+
+});
