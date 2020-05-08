148c952639c56c2781b1e5760e85d3d9a9733c15
Vladimir <avevanguard@gmail.com>
2020-05-08 22:23:09 +0300

    added maps + modal + validation

--- src/js/script.js
+++ src/js/script.js
@@ -22,4 +22,60 @@ $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
     .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
 });
 
+
+
+function toggleSlide(item){
+  $(item).each(function(i){
+    $(this).on('click', function(e){
+      e.preventDefault();
+      $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+      $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+    })
+  });
+};
+
+toggleSlide('.catalog-item__link');
+toggleSlide('.catalog-item__back');
+
+//modal
+ $('[data-modal=consultation]').on('click', function(){
+   $('.overlay, #consultation').fadeIn('slow');
+ });
+ $('.modal__close').on('click',function(){
+   $('.overlay, #consulation, #order, #thanks').fadeOut('slow');
+ });
+
+$('.button_mini').each(function(i){
+  $(this).on('click', function(){
+    $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+    $('.overlay, #order').fadeIn('slow');
+  })
+});
+
+
+function validateForms(form){
+  $(form).validate({
+    rules:{
+      name:"required",
+      phone:"required",
+      email:{
+        required: true,
+        email:true
+      }
+    },
+    messages: {
+      name: "Пожалуйста, введите свое имя",
+      phone: "Пожалуйста, введите свой номер телефона",
+      email: {
+        required: "Пожалуйста, введите свою почту",
+        email: "Неправильно введен адрес почты"
+      }
+    }
+  });
+};
+
+validateForms('#consultation-form');
+validateForms('#consultation form');
+validateForms('#order form');
+
 });
